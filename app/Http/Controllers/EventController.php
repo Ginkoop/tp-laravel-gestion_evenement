97a2d0fac0847ba16d0230e8752cb2948b006a3c
Ginko <jonathan.abergel01@gmail.com>
2025-03-10 17:46:41 +0100

    ajout EventController

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Event;
+use Illuminate\Support\Facades\Auth;
+use App\Mail\ConfirmationInscription;
+use Illuminate\Support\Facades\Mail;
+
+//Mail::to(Auth::user()->email)->send(new ConfirmationInscription($event));
+
+class EventController extends Controller
+{
+    public function index()
+    {
+        $events = Event::latest()->get();
+        return view('events.index', compact('events'));
+    }
+
+    public function create()
+    {
+        return view('events.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required',
+            'date' => 'required|date',
+            'location' => 'required|string',
+        ]);
+
+        Event::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'date' => $request->date,
+            'location' => $request->location,
+            'user_id' => Auth::id(),
+        ]);
+
+        return redirect()->route('events.index')->with('success', 'Événement créé avec succès.');
+    }
+
+    public function show(Event $event)
+    {
+        return view('events.show', compact('event'));
+    }
+
+    public function register(Event $event)
+    {
+        Auth::user()->registrations()->attach($event->id);
+        return back()->with('success', 'Inscription réussie !');
+    }
+
+}
+
